f7ad0ddd203e27993c374d6da8cbf505df042b88
Nuru <ceruks@eteb.me>
2025-10-03 20:16:15 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/socketiochat/main.go
@@ -0,0 +1,23 @@
+// cmd/socketiochat/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "socketiochat/internal/socketiochat"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := socketiochat.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
